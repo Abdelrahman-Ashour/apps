20f8c6d7499d0d54b8378dc055c96347d9cb0785
عبدالرحمن عاشور <137439549+Abdelrahman-Ashour@users.noreply.github.com>
2025-03-16 10:23:17 +0200

    Update startup.py

--- startup.py
+++ startup.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 # إعداد الصفحة
 st.set_page_config(page_title="Startup Idea Finder", layout="wide")
 
-st.title("🚀 Startup Idea Finder")
+st.title("🤖 IDEA.AI")
 st.write("إبحث عن أفضل فكرة لمشروع ناشئ بناءً على مهاراتك وميزانيتك ومستوى المنافسة.")
 
 # إدخال البيانات يدويًا لتجنب الأخطاء

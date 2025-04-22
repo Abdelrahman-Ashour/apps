889acc42f3fbc2e1f67dd5ba411f5993c7fbc243
عبدالرحمن عاشور <137439549+Abdelrahman-Ashour@users.noreply.github.com>
2025-04-22 22:12:14 +0200

    Update IDEA_AI.py

--- IDEA_AI.py
+++ IDEA_AI.py
@@ -10,7 +10,7 @@ import plotly.express as px
 st.set_page_config(page_title="🤖 IDEA.AI", layout="wide")
 
 # اللوجو
-st.image("image\IDEA_AI.png", width=200)
+st.image("image/IDEA_AI.png", width=200)
 st.title("IDEA.AI")
 st.caption("START SMART")
 
